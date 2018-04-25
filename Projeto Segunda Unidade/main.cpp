3422d119360e017fac25fc28ded2748f86b04c6e
RenBertoldo <38046801+RenBertoldo@users.noreply.github.com>
2018-04-25 18:24:49 -0300

    Update inicial

--- Projeto Segunda Unidade/main.cpp
+++ Projeto Segunda Unidade/main.cpp
@@ -12,9 +12,28 @@
 #include "BlackGPIO/BlackGPIO.h"
 #include "ADC/Adc.h"
 
+using namespace BlackLib;
+using namespace std;
 
-// Variáveis Globais
-BlackADC PotSon1(AIN2);
+//Variaveis Globais
+BlackADC PotSon1(AIN0);
 BlackADC PotSon2(AIN6);
 BlackGPIO LedSon1(GPIO_30,output);
 BlackGPIO LedSon2(GPIO_31,output);
+bool parar = false;
+void carga(int k){
+	float f = 0.999999;
+	for(int i=0; i<k; i++){
+		f = f*f*f*f*f;
+		for(int j=0; j<k; j++){
+			f = sin(f)*sin(f)*f*f*f;
+		}
+	}
+}
+void readPot(BlackADC *pot){
+  float var = pot.getNumericValue();
+  while(!parar){
+    
+
+  }
+ }
